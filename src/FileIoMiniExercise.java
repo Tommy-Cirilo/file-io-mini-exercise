a3a977f882296af693be56e3204303d2513b68f1
Tommy.Cirilo <tommy.v.cirilo@gmail.com>
2020-08-26 00:14:24 -0500

    completed io exercise

--- src/FileIoMiniExercise.java
+++ src/FileIoMiniExercise.java
@@ -6,22 +6,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FileIoMiniExercise {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         // TODO: define the path to the instructor-names.txt file
-          Path instructorList = Paths.get("./instructor-names.txt").normalize();
+        Path p = Paths.get("src", "instructor-names.txt");
 
         // TODO: uncomment the following variable declarations
         List<Instructor> instructors;
         List<String> names = new ArrayList<>();
 
         // TODO: read the contents of the instructor-names.txt file and store the list of strings into the 'names' variable
-//        try {
-//            names = Files
-//        }
+        if(Files.exists(p)) {
+            try {
+                names = Files.readAllLines(p);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
         // TODO: assign the 'instructors' variable a list of Instructor objects with names matching the list of strings/names from the text file (the Instructor class has a helpful method for this)
-
+        instructors = Instructor.nameStringsToInstructors(names);
 
         // TODO: greet all instructors by their names
         greetInstructors(instructors);
@@ -29,16 +33,16 @@ public class FileIoMiniExercise {
         // TODO: change "Fred" to "David" in the list of Instructor objects
         changeFredToDavid(instructors);
 
-        // TODO: update the list of strings/names ('names' variable) to the latest names from the
-        //  Instructor objects (the Instructor class has a helpful method for this)
-        names = Instructor.instructorsToNameStrings(instructors)
+        // TODO: update the list of strings/names ('names' variable) to the latest names from the Instructor objects (the Instructor class has a helpful method for this)
+        names = Instructor.instructorsToNameStrings(instructors);
 
         // TODO: overwrite the instructors-names.txt file with the latest list of names
         try {
             Files.write(p, names);
-        } catch (IOException e) {
+        } catch(IOException e) {
             e.printStackTrace();
         }
+
         // TODO: uncomment the following line
         System.out.println("============== after name change...");
 
